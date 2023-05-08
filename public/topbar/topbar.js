a6b28bb3a0395905ac0c106a2064f63947d93f19
Sebastiano Barezzi <seba@sebaubuntu.dev>
2023-05-08 08:24:17 +0200

    ristoranti-storici-api: Migliora la topbar

--- public/topbar/topbar.js
+++ public/topbar/topbar.js
@@ -1,15 +1,50 @@
-const topbar = document.getElementById("topbar");
+const topbarElement = document.getElementById("topbar");
 
-async function main() {
-    let response = await fetch("/topbar/topbar.html");
-    if (!response.ok) {
-        console.error("Errore durante il caricamento della topbar");
-        return;
-    }
-
-    let topbar = await response.text();
-    
-    document.getElementById("topbar").innerHTML = topbar;
+const PAGES = {
+	"Home": "/",
+	"Aggiungi ristorante": "/aggiungi_ristorante.html",
 }
 
-main();
+async function injectTopbar() {
+	topbarElement.innerHTML = `
+		<style>
+			#topbar ul {
+				list-style-type: none;
+				margin: 0;
+				padding: 0;
+				overflow: hidden;
+				background-color: #333;
+			}
+
+			#topbar li {
+				float: left;
+			}
+
+			#topbar li a {
+				display: block;
+				color: white;
+				text-align: center;
+				padding: 14px 16px;
+				text-decoration: none;
+			}
+
+			#topbar li a:hover:not(.active) {
+				background-color: #111;
+			}
+
+			#topbar .active {
+				background-color: #04AA6D;
+			}
+		</style>
+
+		<ul>
+			<li><a>Ristoranti storici</a></li>
+
+			${Object.keys(PAGES).map(page => `
+				<li><a href="${PAGES[page]}" class="${window.location.pathname === PAGES[page] ? "active" : ""}">${page}</a></li>
+			`).join("")}
+		</ul>
+	`;
+}
+
+injectTopbar();
